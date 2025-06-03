d7bb14b5216900a576cd308204e90dfa64b98ce9
IamRam3 <sairamd1905@gmail.com>
2025-06-03 12:58:39 +0530

    adding support for segmentation

--- src/data_setup.py
+++ src/data_setup.py
@@ -3,12 +3,14 @@ import os
 from pycocotools.coco import COCO
 from PIL import Image
 from torchvision import transforms as T
-class COCOObjectDetectionDataset(torch.utils.data.Dataset):
+
+class CocoDataset(torch.utils.data.Dataset, segmentation=False):
     def __init__(self, root, annFile, transforms=None):
         self.root = root
         self.coco = COCO(annFile)
         self.ids = list(sorted(self.coco.imgs.keys()))
         self.transforms = transforms
+        self.segmentation = segmentation
 
     def __getitem__(self, index):
         coco = self.coco
@@ -23,30 +25,65 @@ class COCOObjectDetectionDataset(torch.utils.data.Dataset):
 
         boxes, labels = [], []
 
-        for ann in anns:
-            x, y, w, h = ann['bbox']
-            boxes.append([x, y, x + w, y + h])
-            labels.append(ann['category_id'])
+        if self.segmentation:
+           masks = []
+           for ann in anns:
+              if 'segmentation' not in ann:
+                 continue
+              x, y, w, h = ann['bbox']
+              boxes.append([x, y, x+w, y+h])
+              labels.append(ann['category_id'])
 
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        labels = torch.as_tensor(labels, dtype=torch.int64)
+              mask = coco.annToMask(ann)
+              masks.append(mask)
+           boxes = torch.as_tensor(boxes, dtype=torch.float32)
+           labels = torch.as_tensor(labels, dtype=torch.int64)
+           masks = torch.as_tensor(masks, dtype=torch.uint8)
 
-        image_id = torch.tensor([img_id])
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        iscrowd = torch.zeros((len(anns),), dtype=torch.int64)
+           image_id = torch.tensor([img_id])
+           area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+           iscrowd = torch.zeros((len(anns),), dtype=torch.int64)
 
-        target = {
-            "boxes": boxes,
-            "labels": labels,
-            "image_id": image_id,
-            "area": area,
-            "iscrowd": iscrowd
-        }
+           target = {
+                "boxes": boxes,
+                "labels": labels,
+                "masks": masks,
+                "image_id": image_id,
+                "area": area,
+                "iscrowd": iscrowd
+            }
 
-        if self.transforms:
-            img = self.transforms(img)
+           if self.transforms:
+                img = self.transforms(img)
 
-        return img, target
+           return img, target
+           
+
+        else:
+            for ann in anns:
+                x, y, w, h = ann['bbox']
+                boxes.append([x, y, x + w, y + h])
+                labels.append(ann['category_id'])
+
+            boxes = torch.as_tensor(boxes, dtype=torch.float32)
+            labels = torch.as_tensor(labels, dtype=torch.int64)
+
+            image_id = torch.tensor([img_id])
+            area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+            iscrowd = torch.zeros((len(anns),), dtype=torch.int64)
+
+            target = {
+                "boxes": boxes,
+                "labels": labels,
+                "image_id": image_id,
+                "area": area,
+                "iscrowd": iscrowd
+            }
+
+            if self.transforms:
+                img = self.transforms(img)
+
+            return img, target
 
     def __len__(self):
         return len(self.ids)
@@ -63,18 +100,21 @@ class dataset_dir:
     self.val_annFile = self.val_dir + self.annFile
     self.test_annFile = self.test_dir + self.annFile
   
-  def create_custom_datasets(self, get_transform):
-    train_dataset = COCOObjectDetectionDataset(
+  def create_custom_datasets(self, segmentation, get_transform):
+    train_dataset = CocoDataset(
+        segmentation,
         root=self.train_dir,
         annFile=self.train_annFile,
         transforms=get_transform()
         )
-    val_dataset = COCOObjectDetectionDataset(
+    val_dataset = CocoDataset(
+        segmentation, 
         root=self.val_dir,
         annFile=self.val_annFile,
         transforms=get_transform()
     ) 
-    test_dataset = COCOObjectDetectionDataset(
+    test_dataset = CocoDataset(
+        segmentation, 
         root=self.test_dir,
         annFile=self.test_annFile,
         transforms=get_transform()
